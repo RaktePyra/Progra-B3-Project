a4af61a9afb4dd77edd1a477e56f360d9112f1f6
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-16 18:08:19 +0200

    Added Consistency Game Loop
    -Implemented Consistency Game Loop
    -Added Fixed Update Method used to update system at a fixed interval

--- PrograB3Project/GameEngine.cs
+++ PrograB3Project/GameEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,35 @@ namespace PrograB3Project
     internal class GameEngine
     {
         private bool _shouldExit = false;
+        private Stopwatch _stopwatch = new Stopwatch();
+        private float _loopStartTime = 0;
+        private float _elapsedTime = 0;
+        private float _lag = 0;
+        private const float MS_PER_FRAME = 16;
         public void Run()
         {
+            _stopwatch.Start();
+
             while (!_shouldExit)
             {
+                _loopStartTime = _stopwatch.ElapsedMilliseconds;
                 ProcessInput();
-                Update();
+                while (_lag >= MS_PER_FRAME)
+                {
+                    FixedUpdate(MS_PER_FRAME);
+                    _lag-=MS_PER_FRAME;
+                }
+                Update(_elapsedTime);
                 Render();
+                _elapsedTime = _stopwatch.ElapsedMilliseconds-_loopStartTime;
+                _lag += _elapsedTime;
             }
         }
-        private void Update()
+        private void FixedUpdate(float delta_time)
+        {
+
+        }
+        private void Update(float delta_time)
         {
 
         }
