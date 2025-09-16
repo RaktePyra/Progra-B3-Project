cc5e220e9ae8ad670b2f492c984aff3473020dc5
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-16 17:56:01 +0200

    Create GameEngine.cs
    Added Game Engine class
    -Added Update(), Render() and ProcessInput() methods
    -Added unlimited Game Loop

---
+++ PrograB3Project/GameEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project
+{
+    internal class GameEngine
+    {
+        private bool _shouldExit = false;
+        public void Run()
+        {
+            while (!_shouldExit)
+            {
+                ProcessInput();
+                Update();
+                Render();
+            }
+        }
+        private void Update()
+        {
+
+        }
+        private void Render()
+        {
+
+        }
+        private void ProcessInput()
+        {
+
+        }
+    }
+}
