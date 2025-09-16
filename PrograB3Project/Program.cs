87bc51a1abc6cd73f7ffb4301d73b9f94fdfee44
RaktePyra <167292813+RaktePyra@users.noreply.github.com>
2025-09-16 16:53:28 +0200

    Initial Push
    -Added VS project to repository
    -Configured .gitignore for C#

---
+++ PrograB3Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrograB3Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
