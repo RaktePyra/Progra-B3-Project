2e1d4fa0fc57f57a2bf8bc8c1ae29bddf60b7729
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-19 11:34:31 +0200

    Added constructors to Region + WIP Applying Dependancy inversion to World
    -Added default constructor to Region Class
    -Added a constructors that, by taking an object implementing the IRegion interface
    constructs objects of that type to fill the region table. Has to be tested before use

--- PrograB3Project/Region.cs
+++ PrograB3Project/Region.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace PrograB3Project
 {
-    internal class Regions
+    internal class Region : Components.IRegion
     {
+        private List<City> _cityTable=new List<City>();
+        private const int MAX_CITY_RANDOM_NUMBER = 3;
+        public Region() 
+        {
+            for (int i = 0; i < MAX_CITY_RANDOM_NUMBER; i++)
+            {
+                _cityTable.Add(new City());
+            }
+        }
     }
 }
--- PrograB3Project/World.cs
+++ PrograB3Project/World.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrograB3Project.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace PrograB3Project
 {
-    internal class World
+    public class World 
     {
-        private List<Region> _regionTable=new List<Region>();
+        
+        private List<Region> _regionTable = new List<Region>();
+        private List<Components.IRegion> _IregionTable = new List<Components.IRegion>();
         private const int MAX_REGION_RANDOM_NUMBER = 3;
         //Idea : Create a vector class that can be used as a parameter to this World to specify inside the vector
         //how many cities would be in a region and using the size of the vector as the number of regions inside the world
@@ -16,17 +19,22 @@ namespace PrograB3Project
         {
             for (int i = 0; i < desired_regions_number; i++)
             {
-                _regionTable = new List<Region>();
-            }
-        }  
-        public World()
-        {
-            Random random = new Random();
-            int random_regions_number=random.Next()% MAX_REGION_RANDOM_NUMBER;
-            for (int i = 0; i < random_regions_number; i++)
-            {
-                _regionTable = new List<Region>();
+                _regionTable.Add(new Region());
             }
         }
+        //OOOF, Black Magic incoming to apply Dependy inversion Principle, private till it hasn't been tested thoroughly
+        //Ressources : https://stackoverflow.com/questions/56134343/c-sharp-create-an-instance-of-a-class-from-a-type-fullname
+        //             https://learn.microsoft.com/fr-fr/dotnet/api/system.activator.createinstance?view=net-8.0
+        private World(IRegion region_type_to_create)
+        {
+            Random random = new Random();
+            int random_regions_number = random.Next() % MAX_REGION_RANDOM_NUMBER;
+            Type type = region_type_to_create.GetType();
+            for (int i = 0; i < random_regions_number; i++)
+            {
+                _IregionTable.Add((IRegion)Activator.CreateInstance(type));
+            }
+        }
+        
     }
 }
