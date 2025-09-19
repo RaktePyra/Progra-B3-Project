0a864565129d54f0d19f1cf6839f8f67d0a17f2c
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-19 10:13:35 +0200

    Added Constructors for World Class
    -Added a default constructor that generates a Maximum of 3 regions to store inside of it.
    -Added a constructors that allows to specify the number of regions to generate for the World

--- PrograB3Project/World.cs
+++ PrograB3Project/World.cs
@@ -8,5 +8,25 @@ namespace PrograB3Project
 {
     internal class World
     {
+        private List<Region> _regionTable=new List<Region>();
+        private const int MAX_REGION_RANDOM_NUMBER = 3;
+        //Idea : Create a vector class that can be used as a parameter to this World to specify inside the vector
+        //how many cities would be in a region and using the size of the vector as the number of regions inside the world
+        public World(int desired_regions_number)
+        {
+            for (int i = 0; i < desired_regions_number; i++)
+            {
+                _regionTable = new List<Region>();
+            }
+        }  
+        public World()
+        {
+            Random random = new Random();
+            int random_regions_number=random.Next()% MAX_REGION_RANDOM_NUMBER;
+            for (int i = 0; i < random_regions_number; i++)
+            {
+                _regionTable = new List<Region>();
+            }
+        }
     }
 }
