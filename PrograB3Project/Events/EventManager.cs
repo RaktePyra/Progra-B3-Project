37de874c8c4a50d2cae66df00143e25f5aae7677
RaktePyra <167292813+RaktePyra@users.noreply.github.com>
2025-09-22 13:34:48 +0200

    Added Event Manager Class and Event Class

---
+++ PrograB3Project/Events/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.Events
+{
+    internal class EventManager
+    {
+
+        private Dictionary<Type, List<Action<Event>>> _eventTable;
+
+        public EventManager() 
+        {
+            _eventTable = new Dictionary<Type, List<Action<Event>>>();
+        }
+
+        public void RegisterEvent(Type type, Action<Event> action)
+        {
+            if (!_eventTable.ContainsKey(type))
+            {
+                _eventTable.Add(type, new List<Action<Event>>());
+            }
+            _eventTable[type].Add(action);
+        }
+    }
+}
