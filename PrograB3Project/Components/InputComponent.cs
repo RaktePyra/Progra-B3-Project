691f6e7ec1a310e4351b410306ae726bbf822e54
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-24 09:59:41 +0200

    Created Input Component class that will serve as the entry point for the inputs in the game

---
+++ PrograB3Project/Components/InputComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.Components
+{
+    class InputComponent : Component
+    {
+        public InputComponent(GameObject owner) : base(owner)
+        {
+        }
+
+        public void ProcessInput(ConsoleKeyInfo key)
+        {
+
+        }
+    }
+}
