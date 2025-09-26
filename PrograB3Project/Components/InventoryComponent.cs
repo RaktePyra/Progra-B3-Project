5cb3c675cae3854b37065bfcac620bdcdacbcb32
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-26 10:21:59 +0200

    Created Inventory Component and added logic for storing, adding, removing and getting itemComponents inside

---
+++ PrograB3Project/Components/InventoryComponent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.Components
+{
+    internal class InventoryComponent : Component
+    {
+        private List<ItemComponent> _itemTable = new List<ItemComponent>();
+
+        public InventoryComponent(GameObject owner) : base(owner) 
+        {
+
+        }
+
+        public void AddItem(ItemComponent item)
+        {
+            if(!_itemTable.Contains(item))
+            {
+                _itemTable.Add(item);
+            }
+        }
+
+        public void RemoveItem(ItemComponent item)
+        {
+            if (_itemTable.Contains(item))
+            {
+                _itemTable.Remove(item);
+            }
+        }
+
+        public ItemComponent GetItem(int index)
+        {
+            return _itemTable[index];
+        }
+    }
+}
