fa13fe05f5eff8f371ee143d60545d1c5d612386
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-26 16:42:05 +0200

    Added State Machine to Shop Component to allow for defining multiple behaviours relating to the shop modes like buying or selling

--- PrograB3Project/Components/ShopComponent.cs
+++ PrograB3Project/Components/ShopComponent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrograB3Project.States;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace PrograB3Project.Components
     {
         private TradingComponent _shopTradingComponent;
         private InventoryComponent _shopInventoryComponent;
+        private StateMachine _shopStateMachine;
 
         public ShopComponent(GameObject owner,TradingComponent trading_comp, InventoryComponent shop_inventory_comp) : base(owner)
         {
@@ -19,6 +21,12 @@ namespace PrograB3Project.Components
 
         public override void Enter(GameObject player)
         {
+            player.GetComponent<InputComponent>().BeginInteraction(this);
+        }
+
+        public override void ProcessInput(ConsoleKeyInfo key)
+        {
+            
         }
     }
 }
---
+++ PrograB3Project/States/ShopMainState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.States
+{
+    internal class ShopMainState : IState
+    {
+        public void Enter()
+        {
+          
+        }
+
+        public void Exit()
+        {
+          
+        }
+
+        public void ProcessInput(ConsoleKeyInfo key_info)
+        {
+          
+        }
+
+        public void Render()
+        {
+          
+        }
+
+        public void Update(float delta_time)
+        {
+          
+        }
+    }
+}
