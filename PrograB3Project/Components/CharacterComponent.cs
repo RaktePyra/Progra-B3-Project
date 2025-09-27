857d55eadfc1a4eb40801be8c56b4b37418ed99b
Braham Benjamin <benjaminbraham@outlook.be>
2025-09-27 23:05:43 +0200

    Created Character Component responsible for holding various stats related to a character
    -Added Bargaining stat that will allow to customize shop interaction based on this stat for modifying item prices

---
+++ PrograB3Project/Components/CharacterComponent.cs
@@ -0,0 +1,26 @@
+﻿using PrograB3Project.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.Components
+{
+    internal class CharacterComponent : Component
+    {
+        private int _bargainingStat = 10;
+        public CharacterComponent(GameObject owner, GameEngine engine, EventManager event_manager) : base(owner, engine, event_manager)
+        {
+        }
+
+        public void IncreaseBargainingStat(int number_to_increase_stat)
+        {
+            _bargainingStat += number_to_increase_stat;
+        }
+        public int GetBargainingStat()
+        {
+            return _bargainingStat;
+        }
+    }
+}
