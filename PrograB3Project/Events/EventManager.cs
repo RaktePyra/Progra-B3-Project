6bc85073249cd000bc8e410ee1a631b56063b4e0
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-03 11:46:33 +0200

    Added Queuing for Registering To and Unregistering from event type
    Not using queue and trying to unregister from an event while triggering events would invalidate the iterator from TriggerEvents and would crash the program

--- PrograB3Project/Events/EventManager.cs
+++ PrograB3Project/Events/EventManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,25 +11,31 @@ namespace PrograB3Project.Events
     {
 
         private Dictionary<Type, List<Action<Event>>> _eventTable;
+        private Dictionary<Type, List<Action<Event>>> _eventToRegisterTable;
+        private Dictionary<Type, List<Action<Event>>> _eventToDequeueTable;
 
         public EventManager()
         {
             _eventTable = new Dictionary<Type, List<Action<Event>>>();
+            _eventToDequeueTable = new Dictionary<Type, List<Action<Event>>>();
+            _eventToRegisterTable = new Dictionary<Type, List<Action<Event>>>();
         }
 
         public void RegisterEvent<TYPE>(Action<Event> action) where TYPE : Event
         {
             Type event_type = typeof(TYPE);
 
-            if (!_eventTable.ContainsKey(event_type))
+            if (!_eventToRegisterTable.ContainsKey(event_type))
             {
-                _eventTable.Add(event_type, new List<Action<Event>>());
+                _eventToRegisterTable.Add(event_type, new List<Action<Event>>());
             }
-            _eventTable[event_type].Add(action);
+            _eventToRegisterTable[event_type].Add(action);
         }
 
         public void TriggerEvent(Event event_object)
         {
+            DequeueEventToUnregister();
+            DequeueEventToRegister();
             Type event_type = event_object.GetType();
 
             if (_eventTable.ContainsKey(event_type))
@@ -38,17 +45,50 @@ namespace PrograB3Project.Events
                     action(event_object);
                 }
             }
+            
         }
 
         public void UnregisterFromEvent<TYPE>(Action<Event> action) where TYPE : Event
         {
             Type event_type = typeof(TYPE);
 
-            if (_eventTable.ContainsKey(event_type))
+            if (_eventTable.ContainsKey(event_type) && _eventTable[event_type].Contains(action))
             {
-                _eventTable[event_type].Remove(action);
+               if(!_eventToDequeueTable.ContainsKey(event_type))
+                {
+                    _eventToDequeueTable.Add(event_type, new List<Action<Event>>());
+                    
+                }
+                _eventToDequeueTable[event_type].Add(action);
             }
             
+        }
+
+        public void DequeueEventToUnregister()
+        {
+            foreach(KeyValuePair<Type,List<Action<Event>>> entrie in _eventToDequeueTable)
+            {
+                foreach(Action<Event> action in entrie.Value)
+                {
+                    _eventTable[entrie.Key].Remove(action);
+                }
+            }
+            _eventToDequeueTable.Clear();
+        }
+        public void DequeueEventToRegister()
+        {
+            foreach (KeyValuePair<Type, List<Action<Event>>> entrie in _eventToRegisterTable)
+            {
+                foreach (Action<Event> action in entrie.Value)
+                {
+                    if(!_eventTable.ContainsKey(entrie.Key))
+                    {
+                        _eventTable.Add(entrie.Key, new List<Action<Event>>());
+                    }
+                    _eventTable[entrie.Key].Add(action);
+                }
+            }
+            _eventToRegisterTable.Clear();
         }
     }
 }
