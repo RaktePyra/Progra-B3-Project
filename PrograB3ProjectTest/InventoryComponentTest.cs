fbc37c86c3dfe95424f3461f8936ff6d579d2ce4
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-03 09:42:12 +0200

    Fixed Indentation inside TestProject

--- PrograB3ProjectTest/InventoryComponentTest.cs
+++ PrograB3ProjectTest/InventoryComponentTest.cs
@@ -1,43 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PrograB3Project;
+﻿using PrograB3Project;
 using PrograB3Project.Components;
 using PrograB3Project.Events;
 using PrograB3Project.Interfaces;
 namespace PrograB3ProjectTest
 {
 
-        public class InventoryTests
-        {
-            private InventoryComponent _inventoryToTest;
-            private IGameEngine game_engine = new GameEngine();
-            private IEventManager event_manager = new EventManager();
-            private IGameObject item;
+    public class InventoryTests
+    {
+        private InventoryComponent _inventoryToTest;
+        private IGameEngine game_engine = new GameEngine();
+        private IEventManager event_manager = new EventManager();
+        private IGameObject item;
 
         [SetUp]
-            public void Setup()
-            {
-             item = new GameObject("item",game_engine,event_manager);
-            _inventoryToTest = new InventoryComponent(new GameObject("testObject", game_engine, event_manager),game_engine, event_manager);
-            }
 
-            [Test]
-            public void GetItemCountTest()
-            {
-            _inventoryToTest.AddItem(new ItemComponent(item, game_engine, event_manager,"apple",10,50));
+        public void Setup()
+        {
+            item = new GameObject("item", game_engine, event_manager);
+            _inventoryToTest = new InventoryComponent(new GameObject("testObject", game_engine, event_manager), game_engine, event_manager);
+        }
+
+        [Test]
+
+        public void GetItemCountTest()
+        {
+            _inventoryToTest.AddItem(new ItemComponent(item, game_engine, event_manager, "apple", 10, 50));
 
             Assert.That(_inventoryToTest.GetNumberOfItems(), Is.EqualTo(1));
             _inventoryToTest.AddItem(new ItemComponent(item, game_engine, event_manager, "bread", 10, 50));
             _inventoryToTest.AddItem(new ItemComponent(item, game_engine, event_manager, "water", 10, 50));
             Assert.That(_inventoryToTest.GetNumberOfItems(), Is.EqualTo(3));
-            
-            }
+
+        }
         [Test]
-            public void RemoveItemTest()
-            {
+
+        public void RemoveItemTest()
+        {
 
             _inventoryToTest.AddItem(new ItemComponent(item, game_engine, event_manager, "apple", 10, 50));
 
@@ -49,8 +47,8 @@ namespace PrograB3ProjectTest
             Assert.That(_inventoryToTest.GetNumberOfItems(), Is.EqualTo(2));
         }
         [Test]
-            public void GetItemByIndexTest()
-            {
+        public void GetItemByIndexTest()
+        {
             ItemComponent apple = new ItemComponent(item, game_engine, event_manager, "apple", 10, 50);
             ItemComponent bread = new ItemComponent(item, game_engine, event_manager, "bread", 10, 50);
             ItemComponent water = new ItemComponent(item, game_engine, event_manager, "water", 10, 50);
@@ -63,5 +61,5 @@ namespace PrograB3ProjectTest
             Assert.That(_inventoryToTest.GetItem(3) == null);
 
         }
-        }
+    }
 }
