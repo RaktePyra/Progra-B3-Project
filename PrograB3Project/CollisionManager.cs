fd18ce0e5cec0814109fb717fabfe9dde0960a43
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-04 22:24:16 +0200

    Added Collision Component and Collision Manager classes

---
+++ PrograB3Project/CollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using PrograB3Project.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project
+{
+    internal class CollisionManager
+    {
+        private List<CollisionComponent> _collisionComponentTable = new List<CollisionComponent>();
+    }
+}
---
+++ PrograB3Project/Components/CollisionComponent.cs
@@ -0,0 +1,16 @@
+﻿using PrograB3Project.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.Components
+{
+    internal class CollisionComponent : Component
+    {
+        public CollisionComponent(IGameObject owner, IGameEngine engine, IEventManager event_manager, RenderManager render_manager) : base(owner, engine, event_manager, render_manager)
+        {
+        }
+    }
+}
