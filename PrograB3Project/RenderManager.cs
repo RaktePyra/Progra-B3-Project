09776d418801ae97542d35b2f70dce69fdaac66d
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-04 12:02:26 +0200

    Added Logic for Rendering Elements inside RenderManager

--- PrograB3Project/RenderManager.cs
+++ PrograB3Project/RenderManager.cs
@@ -13,7 +13,27 @@ namespace PrograB3Project
         
         public void Render()
         {
+            Console.Clear();
+            foreach (RenderComponent render_component in _renderComponentsTable)
+            {
+                render_component.Render();
+            }
+        }
 
+        public void RegisterRenderComponent(RenderComponent component_to_register)
+        {
+            if (component_to_register != null && !_renderComponentsTable.Contains(component_to_register))
+            {
+                _renderComponentsTable.Add(component_to_register);
+            }
+        }
+
+        public void UnregisterRenderComponent(RenderComponent component_to_unregister)
+        {
+            if (_renderComponentsTable.Contains(component_to_unregister))
+            {
+                _renderComponentsTable.Remove(component_to_unregister);
+            }
         }
     }
 }
