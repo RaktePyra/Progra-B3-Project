d4bf3b79c1caa13ae1e125c4d870f61e4fbf0c16
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-05 18:21:41 +0200

    Added logic for registering, unregistering and checking for collision components

--- PrograB3Project/CollisionManager.cs
+++ PrograB3Project/CollisionManager.cs
@@ -7,8 +7,41 @@ using System.Threading.Tasks;
 
 namespace PrograB3Project
 {
-    internal class CollisionManager
+    public class CollisionManager
     {
         private List<CollisionComponent> _collisionComponentTable = new List<CollisionComponent>();
+
+
+        public CollisionComponent GetCollisionComponentAtLocation(LocationCoordinates location)
+        {
+            CollisionComponent component_to_return = null;
+
+            foreach(CollisionComponent component in _collisionComponentTable)
+            {
+                if(component.GetTransform()._xCoordinate == location._xCoordinate && component.GetTransform()._yCoordinate == location._yCoordinate)
+                {
+                    component_to_return = component;
+                    break;
+                }
+            }
+
+            return component_to_return;
+        }
+
+        public void RegisterCollisionComponent(CollisionComponent collision_component)
+        {
+            if(!_collisionComponentTable.Contains(collision_component))
+            {
+                _collisionComponentTable.Add(collision_component);
+            }
+        }
+
+        public void UnregisterCollisionComponent(CollisionComponent collision_component)
+        {
+            if (_collisionComponentTable.Contains(collision_component))
+            {
+                _collisionComponentTable.Remove(collision_component);
+            }
+        }
     }
 }
