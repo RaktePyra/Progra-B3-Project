76ea766989d92c59f643acda6b13742acc8483e7
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-10 19:20:13 +0200

    Added new SavableComponentRegisterEvent

---
+++ PrograB3Project/Events/SavableComponentRegisteredEvent.cs
@@ -0,0 +1,24 @@
+﻿using PrograB3Project.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.Events
+{
+    public class SavableComponentRegisteredEvent : Event
+    {
+        private ISavableComponent _savableComponent;
+
+        public SavableComponentRegisteredEvent(ISavableComponent component) 
+        {
+            _savableComponent = component;
+        }
+
+        public ISavableComponent GetComponent()
+        {
+            return _savableComponent;
+        }
+    }
+}
