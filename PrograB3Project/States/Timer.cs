1bd19023150d2baaebf4e9b9f0d3df2aa41e512f
Braham Benjamin <benjaminbraham@outlook.be>
2025-10-10 17:36:49 +0200

    Added Timer class responsible for triggering provided action after a certain time

---
+++ PrograB3Project/States/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograB3Project.States
+{
+    public class Timer
+    {
+        private Action _actionToTrigger;
+        private float _triggerTime;
+        private float _internalTimer = 0;
+        private bool _hasBeenStarted = false;
+        public Timer(Action delegate_to_call, float time_in_seconds)
+        {
+            _actionToTrigger = delegate_to_call;
+            _triggerTime = time_in_seconds;
+        }
+        
+        public void Start()
+        {
+            _hasBeenStarted = true;
+        }
+        public void Update(float delta_time)
+        {
+            if (_hasBeenStarted)
+            {
+                _internalTimer += delta_time;
+
+                if (_internalTimer > _triggerTime)
+                {
+                    _actionToTrigger();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            _hasBeenStarted = false;
+            _internalTimer = 0;
+        }
+    }
+}
