9710145eaae2cff07ff654a79c8efe435991f2ab
Braham Benjamin <benjaminbraham@outlook.be>
2025-11-22 16:43:52 +0100

    Added ISavableComponent to CharacterComponent

--- PrograB3Project/Components/CharacterComponent.cs
+++ PrograB3Project/Components/CharacterComponent.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrograB3Project.Interfaces;
 
 namespace PrograB3Project.Components
 {
-    internal class CharacterComponent : Component
+    internal class CharacterComponent : Component, ISavableComponent
     {
         private int _bargainingStat = 10;
-
+        private string _id;
         public CharacterComponent(GameObject owner) : base(owner)
         {
+            _id = owner.GetName()+"_CharacterComponent";
         }
 
         public void IncreaseBargainingStat(int number_to_increase_stat)
@@ -23,6 +25,21 @@ namespace PrograB3Project.Components
         public int GetBargainingStat()
         {
             return _bargainingStat;
+        }
+
+        public string Save()
+        {
+            return _bargainingStat.ToString();
+        }
+
+        public void RestoreDataFromFile(string data)
+        {
+          int.TryParse(data, out _bargainingStat);
+        }
+
+        public string GetID()
+        {
+            return _id;
         }
     }
 }
